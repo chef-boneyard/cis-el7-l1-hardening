4e75aa2257fef2421e65eaa5645da7278cb60347
Simon Fisher <simfish85@googlemail.com>
2016-04-26 12:15:49 +0100

    Updated default recipe to install and enable firewall

--- hardeningCookbooks/sa-hardening/recipes/default.rb
+++ hardeningCookbooks/sa-hardening/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Fix for "xccdf_org.cisecurity.benchmarks_rule_4.7_Enable_firewalld"
+
+package 'firewalld'
+
+service 'firewalld' do
+  supports :status => true
+  action [ :enable, :start ]
+end
+
+# End fix for "xccdf_org.cisecurity.benchmarks_rule_4.7_Enable_firewalld"

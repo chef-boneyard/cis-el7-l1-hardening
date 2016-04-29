ac8af08e6d9839e22c98a712bb70e22bc45cf788
Joe Gardiner <joe@grdnr.io>
2016-04-29 20:51:37 +0100

    Moved SSH remediation to dedicated recipe
    -----> Starting Kitchen (v1.5.0)
    -----> Converging <default-rhel-7>...
    Preparing files for transfer
    Preparing dna.json
    Resolving cookbook dependencies with Berkshelf 4.2.1...
    Removing non-cookbook files before transfer
    Preparing solo.rb
    -----> Chef Omnibus installation detected (install only if missing)
    Transferring files to <default-rhel-7>
    Starting Chef Client, version 12.9.38
    Installing Cookbook Gems:
    Compiling Cookbooks...
    Converging 17 resources
    Recipe: sa-hardening::enable_sudo_no_tty
    * delete_lines[remove hash-comments from /some/file] action edit (up to
    date)
    Recipe: sa-hardening::ssh
    * replace_or_add[SSH Protocol] action edit (up to date)
    * replace_or_add[SSH MaxAuthTries] action edit (up to date)
    * replace_or_add[Disable SSH X11 Forwarding] action edit (up to date)
    * replace_or_add[Set SSH IgnoreRhosts to Yes] action edit (up to date)
    Recipe: sa-hardening::default
    * yum_package[firewalld] action install (up to date)
    * service[firewalld] action enable (up to date)
    * service[firewalld] action start (up to date)
    * directory[/etc/cron.d] action create (up to date)
    * directory[/etc/cron.monthly] action create (up to date)
    * directory[/etc/cron.weekly] action create (up to date)
    * directory[/etc/cron.daily] action create (up to date)
    * directory[/etc/cron.hourly] action create (up to date)
    * file[/etc/crontab] action create (up to date)
    * file[/etc/anacrontab] action create (up to date)
    * file[/etc/passwd] action create (up to date)
    * file[/boot/grub2/grub.cfg] action create (up to date)
    * replace_or_add[Restrict su Command] action edit (up to date)
    
    Running handlers:
    Running handlers complete
    Chef Client finished, 0/18 resources updated in 02 seconds
    Finished converging <default-rhel-7> (0m12.81s).
    -----> Kitchen is finished. (0m17.43s)

--- hardeningCookbooks/sa-hardening/recipes/default.rb
+++ hardeningCookbooks/sa-hardening/recipes/default.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+include_recipe 'sa-hardening::ssh'
+
 # Fix for "xccdf_org.cisecurity.benchmarks_rule_4.7_Enable_firewalld"
 package 'firewalld'
 
@@ -48,14 +50,6 @@ file '/boot/grub2/grub.cfg' do
 end
 # End fix for xccdf_org.cisecurity.benchmarks_rule_1.5.2_Set_Permissions_on_bootgrub2grub.cfg
 
-# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.1_Set_SSH_Protocol_to_2
-replace_or_add "SSH Protocol" do
-  path "/etc/ssh/sshd_config"
-  pattern "Protocol 1"
-  line "Protocol 2"
-end
-# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.1_Set_SSH_Protocol_to_2
-
 # Start fix for xccdf_org.cisecurity.benchmarks_rule_6.5_Restrict_Access_to_the_su_Command
 replace_or_add "Restrict su Command" do
   path "/etc/pam.d/su"
@@ -63,27 +57,3 @@ replace_or_add "Restrict su Command" do
   line "auth            required        pam_wheel.so use_uid"
 end
 # End xccdf_org.cisecurity.benchmarks_rule_6.5_Restrict_Access_to_the_su_Command
-
-# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.5_Set_SSH_MaxAuthTries_to_4_or_Less
-replace_or_add "SSH MaxAuthTries" do
-  path "/etc/ssh/sshd_config"
-  pattern "MaxAuthTries.*"
-  line "MaxAuthTries 4"
-end
-# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.5_Set_SSH_MaxAuthTries_to_4_or_Less
-
-# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.4_Disable_SSH_X11_Forwarding
-replace_or_add "Disable SSH X11 Forwarding" do
-  path "/etc/ssh/sshd_config"
-  pattern "X11Forwarding.*"
-  line "X11Forwarding no"
-end
-# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.4_Disable_SSH_X11_Forwarding
-
-# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.6_Set_SSH_IgnoreRhosts_to_Yes
-replace_or_add "Set SSH IgnoreRhosts to Yes" do
-  path "/etc/ssh/sshd_config"
-  pattern "IgnoreRhosts.*"
-  line "IgnoreRhosts yes"
-end
-# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.6_Set_SSH_IgnoreRhosts_to_Yes
---
+++ hardeningCookbooks/sa-hardening/recipes/ssh.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: sa-hardening
+# Recipe:: ssh
+# Remediation for SSH specific controls
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.1_Set_SSH_Protocol_to_2
+replace_or_add "SSH Protocol" do
+  path "/etc/ssh/sshd_config"
+  pattern "Protocol 1"
+  line "Protocol 2"
+end
+# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.1_Set_SSH_Protocol_to_2
+
+# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.5_Set_SSH_MaxAuthTries_to_4_or_Less
+replace_or_add "SSH MaxAuthTries" do
+  path "/etc/ssh/sshd_config"
+  pattern "MaxAuthTries.*"
+  line "MaxAuthTries 4"
+end
+# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.5_Set_SSH_MaxAuthTries_to_4_or_Less
+
+# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.4_Disable_SSH_X11_Forwarding
+replace_or_add "Disable SSH X11 Forwarding" do
+  path "/etc/ssh/sshd_config"
+  pattern "X11Forwarding.*"
+  line "X11Forwarding no"
+end
+# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.4_Disable_SSH_X11_Forwarding
+
+# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.6_Set_SSH_IgnoreRhosts_to_Yes
+replace_or_add "Set SSH IgnoreRhosts to Yes" do
+  path "/etc/ssh/sshd_config"
+  pattern "IgnoreRhosts.*"
+  line "IgnoreRhosts yes"
+end
+# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.6_Set_SSH_IgnoreRhosts_to_Yes

ae35578f47911bd3de93f0c693f70913c1bc8058
Joe Gardiner <joe@grdnr.io>
2016-04-29 14:30:46 +0100

    Fixed SSH Protocol should equal 2 xccdf_org.cisecurity.benchmarks_rule_6.2.1_Set_SSH_Protocol_to_2

--- hardeningCookbooks/sa-hardening/recipes/default.rb
+++ hardeningCookbooks/sa-hardening/recipes/default.rb
@@ -5,14 +5,12 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 # Fix for "xccdf_org.cisecurity.benchmarks_rule_4.7_Enable_firewalld"
-
 package 'firewalld'
 
 service 'firewalld' do
   supports :status => true
   action [ :enable, :start ]
 end
-
 # End fix for "xccdf_org.cisecurity.benchmarks_rule_4.7_Enable_firewalld"
 
 # Start fix for hardening of cronfiles
@@ -49,3 +47,11 @@ file '/boot/grub2/grub.cfg' do
   group 'root'
 end
 # End fix for xccdf_org.cisecurity.benchmarks_rule_1.5.2_Set_Permissions_on_bootgrub2grub.cfg
+
+# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.2.1_Set_SSH_Protocol_to_2
+replace_or_add "SSH Protocol" do
+  path "/etc/ssh/sshd_config"
+  pattern "Protocol 1"
+  line "Protocol 2"
+end
+#End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.1_Set_SSH_Protocol_to_2

9e7cc33e90966ce905c66e887202e8708fc32ed1
Yvo van Doorn <yvo@chef.io>
2016-04-29 22:51:37 +0200

    Add more guards to ensure it only executes on RHEL 7, relax platform to platform_family when enforcing by version 7

--- hardeningCookbooks/sa-hardening/recipes/avahi.rb
+++ hardeningCookbooks/sa-hardening/recipes/avahi.rb
@@ -3,33 +3,39 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-package 'Install Avahi Autoipd' do
-  case node["platform"]
-  when 'redhat'
-    package_name 'avahi-autoipd'
+package 'Install Avahi Libs' do
+  case node["platform_family"]
+  when 'rhel'
+    if node['platform_version'].to_f >= 7.0
+      package_name 'avahi-libs'
+    end
   end
   action :install
 end
 
-package 'Install Avahi Libs' do
-  case node["platform"]
-  when 'redhat'
-    package_name 'avahi-libs'
+package 'Install Avahi Autoipd' do
+  case node["platform_family"]
+  when 'rhel'
+    if node['platform_version'].to_f >= 7.0
+      package_name 'avahi-autoipd'
+    end
   end
   action :install
 end
 
 package 'Install Avahi' do
-  case node["platform"]
-  when 'redhat'
-    package_name 'avahi'
+  case node["platform_family"]
+  when 'rhel'
+    if node['platform_version'].to_f >= 7.0
+      package_name 'avahi'
+    end
   end
   action :install
 end
 
 service 'avahi-daemon.socket' do
-  case node["platform"]
-  when 'redhat'
+  case node["platform_family"]
+  when 'rhel'
     if node['platform_version'].to_f >= 7.0
       provider Chef::Provider::Service::Systemd
     end
@@ -38,8 +44,8 @@ service 'avahi-daemon.socket' do
 end
 
 service 'avahi-daemon.service' do
-  case node["platform"]
-  when 'redhat'
+  case node["platform_family"]
+  when 'rhel'
     if node['platform_version'].to_f >= 7.0
       provider Chef::Provider::Service::Systemd
     end

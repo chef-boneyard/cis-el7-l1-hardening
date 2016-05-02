18b840da13257471e49c98307c503b626fce9afd
Yvo van Doorn <yvo@chef.io>
2016-05-02 09:15:57 +0200

    Move locking down of passwd file and su ability to user-mgmt.rb

--- hardeningCookbooks/sa-hardening/recipes/user-mgmt.rb
+++ hardeningCookbooks/sa-hardening/recipes/user-mgmt.rb
@@ -14,4 +14,20 @@ when 'rhel'
   end
 # End xccdf_org.cisecurity.benchmarks_rule_7.5_Lock_Inactive_User_Accounts
 
+# Start fix for xccdf_org.cisecurity.benchmarks_rule_9.1.2_Verify_Permissions_on_etcpasswd
+  file '/etc/passwd' do
+    mode '0644'
+    owner 'root'
+    group 'root'
+  end
+# End fix for xccdf_org.cisecurity.benchmarks_rule_9.1.2_Verify_Permissions_on_etcpasswd
+
+# Start fix for xccdf_org.cisecurity.benchmarks_rule_6.5_Restrict_Access_to_the_su_Command
+  replace_or_add "Restrict su Command" do
+    path "/etc/pam.d/su"
+    pattern ".*pam_wheel.so use_uid"
+    line "auth            required        pam_wheel.so use_uid"
+  end
+# End xccdf_org.cisecurity.benchmarks_rule_6.5_Restrict_Access_to_the_su_Command
+
 end

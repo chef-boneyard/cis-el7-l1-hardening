2cc4bad5a538fbcd7de7e4f737a668fc2e5cef12
Yvo van Doorn <yvo@chef.io>
2016-05-03 14:04:49 +0100

    Satisfy Foodcritic rules

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
 name 'cis-el7-l1-hardening'
 maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'emea-saleseng@chef.io'
 license 'all_rights'
 description 'Installs/Configures cis-el7-l1-hardening'
 long_description 'Installs/Configures cis-el7-l1-hardening'
 version '0.2.0'
+issues_url 'https://github.com/chef-customers/cis-el7-l1-hardening/issues'
+source_url 'https://github.com/chef-customers/cis-el7-l1-hardening'
 
 depends 'line', '~> 0.6.3'

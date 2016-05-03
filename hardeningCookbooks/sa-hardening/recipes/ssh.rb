b271344e3c63f97e375831488bc488b4f4498f31
Thomas Robinson-Gore <trg@chef.io>
2016-05-03 10:37:52 +0100

    Add fix for issue #23 Environment Options

--- hardeningCookbooks/sa-hardening/recipes/ssh.rb
+++ hardeningCookbooks/sa-hardening/recipes/ssh.rb
@@ -95,3 +95,11 @@ replace_or_add "Set Idle Timeout Interval for User Login - ClientAliveCountMax" 
   line "ClientAliveCountMax 0"
 end
 # End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.12_Set_Idle_Timeout_Interval_for_User_Login
+
+# xccdf_org.cisecurity.benchmarks_rule_6.2.10_Do_Not_Allow_Users_to_Set_Environment_Options
+replace_or_add "Do Not Allow Users to Set Environment Options" do
+  path "/etc/ssh/sshd_config"
+  pattern "PermitUserEnvironment.*"
+  line "PermitUserEnvironment no"
+end
+# End fix for xccdf_org.cisecurity.benchmarks_rule_6.2.10_Do_Not_Allow_Users_to_Set_Environment_Options

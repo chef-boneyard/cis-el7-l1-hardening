cce09b9626160b73649f095e430b616ffc7da383
Thomas Robinson-Gore <trg@chef.io>
2016-05-04 16:33:39 +0100

    Amend recipes to fix some rubocop offenses

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -7,7 +7,7 @@ case node['platform_family']
 when 'rhel'
   if node['platform_version'].to_f >= 7.0
 
-# Begin xccdf_org.cisecurity.benchmarks_rule_6.1.2_Enable_crond_Daemon
+    # Begin xccdf_org.cisecurity.benchmarks_rule_6.1.2_Enable_crond_Daemon
     package 'Install cronie' do
       package_name 'cronie'
       action :install
@@ -17,9 +17,9 @@ when 'rhel'
       provider Chef::Provider::Service::Systemd
       action [:enable, :start]
     end
-# End xccdf_org.cisecurity.benchmarks_rule_6.1.2_Enable_crond_Daemon
+    # End xccdf_org.cisecurity.benchmarks_rule_6.1.2_Enable_crond_Daemon
 
-# Begin xccdf_org.cisecurity.benchmarks_rule_6.1.1_Enable_anacron_Daemon
+    # Begin xccdf_org.cisecurity.benchmarks_rule_6.1.1_Enable_anacron_Daemon
 
     package 'Install cronie-anacron' do
       package_name 'cronie-anacron'
@@ -30,17 +30,17 @@ when 'rhel'
       package_name 'crontabs.noarch'
       action :install
     end
-# End xccdf_org.cisecurity.benchmarks_rule_6.1.1_Enable_anacron_Daemon
+    # End xccdf_org.cisecurity.benchmarks_rule_6.1.1_Enable_anacron_Daemon
 
-# Start fix for hardening of cronfiles
+    # Start fix for hardening of cronfiles
     ['/etc/cron.d', '/etc/cron.monthly', '/etc/cron.weekly',
      '/etc/cron.daily', '/etc/cron.hourly'].each do |crondir|
-       directory crondir do
+      directory crondir do
         mode '0700'
         owner 'root'
         group 'root'
         action :create
-       end
+      end
     end
 
     ['/etc/crontab', '/etc/anacrontab'].each do |cronfile|
@@ -51,9 +51,9 @@ when 'rhel'
         action :create
       end
     end
-# End fix for hardening of cronfiles
+    # End fix for hardening of cronfiles
 
-# Begin xccdf_org.cisecurity.benchmarks_rule_6.1.11_Restrict_atcron_to_Authorized_Users
+    # Begin xccdf_org.cisecurity.benchmarks_rule_6.1.11_Restrict_atcron_to_Authorized_Users
     file '/etc/cron.deny' do
       action :delete
     end
@@ -64,7 +64,7 @@ when 'rhel'
       owner 'root'
       group 'root'
     end
-# End xccdf_org.cisecurity.benchmarks_rule_6.1.11_Restrict_atcron_to_Authorized_Users
+    # End xccdf_org.cisecurity.benchmarks_rule_6.1.11_Restrict_atcron_to_Authorized_Users
 
   end
 end

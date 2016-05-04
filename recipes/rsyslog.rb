acdf8627337af6b4aba472c4cf3e7fc76cb0778a
--get <yvo@chef.io>
2016-05-04 13:57:04 +0100

    Tell rsyslog to restart immediately

--- recipes/rsyslog.rb
+++ recipes/rsyslog.rb
@@ -18,6 +18,6 @@ replace_or_add 'Configure rsyslog to send logs to remote host' do
   path '/etc/rsyslog.conf'
   pattern "#*.* @@remote-host:514"
   line "*.* @@remote-host:514"
-  notifies :restart, "service[rsyslog.service]"
+  notifies :restart, 'service[rsyslog.service]', :immediately
 end
 # End fix for xccdf_org.cisecurity.benchmarks_rule_5.1.5_Configure_rsyslog_to_Send_Logs_to_a_Remote_Log_Host

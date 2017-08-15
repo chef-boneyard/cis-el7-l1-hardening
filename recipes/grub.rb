d1abb52e7bd7b00cb89d9a014354df54867cba61
modrake <morgan.s.drake@gmail.com>
2017-08-15 11:35:18 -0700

    Standardized licensing on recipes
    Signed-off-by: modrake <morgan.s.drake@gmail.com>

--- recipes/grub.rb
+++ recipes/grub.rb
@@ -1,7 +1,19 @@
 # Cookbook Name:: cis-el7-l1-hardening
 # Recipe:: grub
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright:: 2017, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 # Start fix for xccdf_org.cisecurity.benchmarks_rule_1.5.2_Set_Permissions_on_bootgrub2grub.cfg

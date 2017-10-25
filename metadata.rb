ba01cc4d168898c90f70d71fc79009507e3e6420
Tim Smith <tsmith@chef.io>
2017-10-25 15:49:26 -0700

    Add chef_version and supports to metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,8 @@ long_description 'Installs/Configures cis-el7-l1-hardening'
 issues_url 'https://github.com/chef-cookbooks/cis-el7-l1-hardening/issues'
 source_url 'https://github.com/chef-cookbooks/cis-el7-l1-hardening'
 version '0.7.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 
+supports 'redhat'
+supports 'centos'
 depends 'line', '~> 0.6.3'

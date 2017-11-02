198a7d22e06ce6a55187c609ce4a7f932ac94d86
Matt Ray <matthewhray@gmail.com>
2017-11-02 13:15:17 +1100

    3.2.3_Ensure_secure_ICMP_redirects_are_not_accepted: Ensure secure ICMP redirects are not accepted
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/network-packet-remediation.rb
+++ recipes/network-packet-remediation.rb
@@ -79,6 +79,16 @@ when 'rhel'
     pattern 'net.ipv4.conf.default.accept_redirects'
     line 'net.ipv4.conf.default.accept_redirects=0'
   end
+  replace_or_add 'enable_net.ipv4.conf.all.secure_redirects=0' do
+    path '/etc/sysctl.conf'
+    pattern 'net.ipv4.conf.all.secure_redirects'
+    line 'net.ipv4.conf.all.secure_redirects=0'
+  end
+  replace_or_add 'enable_net.ipv4.conf.default.secure_redirects=0' do
+    path '/etc/sysctl.conf'
+    pattern 'net.ipv4.conf.default.secure_redirects'
+    line 'net.ipv4.conf.default.secure_redirects=0'
+  end
   execute 'update_net.ipv4.conf.all.accept_redirects=0' do
     command '/sbin/sysctl -w net.ipv4.conf.all.accept_redirects=0'
     not_if '/sbin/sysctl -q -n net.ipv4.conf.all.accept_redirects | /usr/bin/grep 0'
@@ -86,6 +96,14 @@ when 'rhel'
   execute 'update_net.ipv4.conf.default.accept_redirects=0' do
     command '/sbin/sysctl -w net.ipv4.conf.default.accept_redirects=0'
     not_if '/sbin/sysctl -q -n net.ipv4.conf.default.accept_redirects | /usr/bin/grep 0'
+  end
+  execute 'update_net.ipv4.conf.all.secure_redirects=0' do
+    command '/sbin/sysctl -w net.ipv4.conf.all.secure_redirects=0'
+    not_if '/sbin/sysctl -q -n net.ipv4.conf.all.secure_redirects | /usr/bin/grep 0'
+  end
+  execute 'update_net.ipv4.conf.default.secure_redirects=0' do
+    command '/sbin/sysctl -w net.ipv4.conf.default.secure_redirects=0'
+    not_if '/sbin/sysctl -q -n net.ipv4.conf.default.secure_redirects | /usr/bin/grep 0'
   end
   # End ICMP Redirect Acceptance
 

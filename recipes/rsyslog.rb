a2ee52bb16d89016cd330124b2d879ce2a5f6a6e
Matt Ray <matthewhray@gmail.com>
2017-11-08 15:17:56 +1100

    Ensure rsyslog default file permissions configured
    xccdf_org.cisecurity.benchmarks_rule_4.2.1.3_Ensure_rsyslog_default_file_permissions_configured
    
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/rsyslog.rb
+++ recipes/rsyslog.rb
@@ -21,3 +21,12 @@ replace_or_add 'Configure rsyslog to send logs to remote host' do
   notifies :restart, 'service[rsyslog.service]', :immediately
 end
 # End fix for xccdf_org.cisecurity.benchmarks_rule_5.1.5_Configure_rsyslog_to_Send_Logs_to_a_Remote_Log_Host
+
+# xccdf_org.cisecurity.benchmarks_rule_4.2.1.3_Ensure_rsyslog_default_file_permissions_configured
+replace_or_add 'Ensure rsyslog default file permissions configured' do
+  path '/etc/rsyslog.conf'
+  pattern '\$FileCreateMode'
+  line '$FileCreateMode 0600'
+  notifies :restart, 'service[rsyslog.service]', :immediately
+end
+# End fix for xccdf_org.cisecurity.benchmarks_rule_4.2.1.3_Ensure_rsyslog_default_file_permissions_configured

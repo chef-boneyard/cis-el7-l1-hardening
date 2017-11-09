8903a73d44071edf64c06b1307d89b119953ed19
Matt Ray <matthewhray@gmail.com>
2017-11-09 13:08:51 +1100

    1.3.2_Ensure_filesystem_integrity_is_regularly_checked
    Add daily aide --check
    
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/aide.rb
+++ recipes/aide.rb
@@ -10,3 +10,10 @@ package 'Install AIDE' do
   package_name 'aide'
   action :install
 end
+
+# xccdf_org.cisecurity.benchmarks_rule_1.3.2_Ensure_filesystem_integrity_is_regularly_checked:
+replace_or_add "Ensure filesystem integrity is regularly checked" do
+  path '/etc/crontab'
+  pattern 'aide --check'
+  line '05 3 * * * root /usr/sbin/aide --check'
+end
